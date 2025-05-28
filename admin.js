ae31343a92e02f6a5c191775916266ecb71f5832
Ariuka <ariunbayar446@gmail.com>
2025-05-28 18:36:35 +0200

    Anhnii commit

---
+++ admin.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    username: "bagsh1",
+    password: "1234",
+    role: "teacher",
+    name: "Ганбаатар багш"
+  },
+  {
+    username: "suragch1",
+    password: "abcd",
+    role: "student",
+    name: "Сараа",
+    grades: {
+      "Математик": 92,
+      "Англи хэл": 85
+    },
+    attendance: {
+      "2025-05-27": true,
+      "2025-05-26": false
+    }
+  }
+];
+
+
+
